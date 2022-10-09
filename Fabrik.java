366f36f9231dd9003d1172dd2246a2060f2ecf8c
chamueller98 <54662565+chamueller98@users.noreply.github.com>
2022-10-09 22:03:27 +0200

    Update Fabrik.java

--- Fabrik.java
+++ Fabrik.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
  */
 public class Fabrik
 {
-    // Instanzen gemäss MUsterlösung
+    // Instanzen gemäss Musterlösung
+    // Feedback Cha: ich würde den untenstehenden Kommentar (und allgemein?) immer oberhalb des Codes machen, für weniger Verwirrung)
     private ArrayList<Bestellung> bestellungen; //basierend auf Typ Bestellung machen wir eine Liste namens bestellungen
     private int bestellungsNr;
 
@@ -34,6 +35,16 @@ public class Fabrik
         //
     }
     
+    /** 
+    * Feedback Cha: Warum ist "chairs" auf Englisch? Und warum sofa im Singular? Ich würde beides auf Deutsch und entweder sg oder pl. :)
+    * Sind die Kommentare innerhalb des Blocks (den geschweiften Klammern) nicht etwas verwirrend?
+    * Wenn man nun eine Bestellung aufgibt, kann man hier schon angeben, ob es Stühle oder Sofas sein sollen? 
+    * sagt die Funktion "bestellungen.add(new Bestellung());" dem System das? Oder muss man evtl. mit einer weiteren Funktion vorher angeben, was bestellt wird? 
+    * vielleicht so: 
+    * bestellungsNr = bestellungsNr+1;
+    * Bestellung eineBestellung = new Bestellung(bestellungsNr, sofa, chairs);
+    * bestellungen.add(new Bestellung);
+    */
     public void bestellungAufgeben(int sofa, int chairs)
     {
            //Bestellung aufgeben -->referenziert Klasse Bestellung und called Konstruktorfunktion von Bestellung
@@ -51,12 +62,28 @@ public class Fabrik
     //{
       //  notizen.add(notiz);
     //}
+    
+    /**
+    * Feedback Cha
+    * Hier muss meiner Meinung rein, was geprintet werden soll. Untenstehend mein Input:
+    * System.out.println("Bestellnummer: " + bestellung.gibBestellungsNr());
+    * System.out.println("Anzahl Stühle: " + bestellung.gibAnzahlStuehle()); --> hier schauen, dass Stuehle konsistent geschrieben ist
+    * System.out.println("Anzahl Sofas: " + bestellung.gibAnzahlSofas()); --> hier schauen, dass Sofas konsistent geschrieben ist
+    * System.out.println("Beschaffungszeit: " + bestellung.gibBeschaffungsZeit());
+    * System.out.println("Bestellbestätigung: " + bestellung.gibBestellBestaetigung());
+    * System.out.println();
+    */
     public void bestellungAusgeben()
     {
         //for(Klasse_BeschreibenderNamefür diese lokale Variable: in Liste bestellungen
         //Jedes El. der Liste "bestellungen" wird unter eineBestellung gespeichert und anschliessend geprintet
-        for(Bestellung eineBestellung: bestellungen){
+        for(Bestellung eineBestellung: bestellungen) {
             System.out.println(eineBestellung);
         }
     }
+     /**
+    * Feedback Cha
+    * braucht es noch eine Methode um das Programm zu starten?
+    */
+    
 }
